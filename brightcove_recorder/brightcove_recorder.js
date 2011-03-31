46ec7eacd90db845adfb9d39706c65a4b9e18c67
Dave Hall <dave.hall@skwashd.com>
2011-03-31 21:50:21 +1100

    add brightcove recorder. a module for integrating video_recorder with the brightcove_cck module

---
+++ brightcove_recorder/brightcove_recorder.js
@@ -0,0 +1,34 @@
+Drupal.behaviors.brightcoveRecorderAttachActions = function(context) {
+  $('input.brightcove-recorder-record-button:not(.processed)', context)
+    .each(function() {
+      $(this).click(function() {
+        var button = $(this),
+          idParts = button.attr('id').match(/edit-(.+)-(\d+)-record-(.+)/),
+          delta = idParts[2],
+          videoId = idParts[3];
+        Drupal.modalFrame.open({url: Drupal.settings.basePath + 'video_recorder/record/popup/' + videoId, width: 700});
+        return false;
+      })
+      .addClass('processed');
+    });
+
+  $('object.video-recorder:not(processed)', context)
+    .each(function() {
+      $(this).bind('video-saved', {}, function(e, data) {
+        var elm = $(this),
+	  videoId = data,
+	  requestURL = Drupal.settings.basePath + 'brightcove_recorder/upload/' + videoId;
+	  $.getJSON(requestURL, function(data) {
+	    if (data.brightcove_id) {
+	      var target = $('input.brightcove-video-' + videoId, window.parent.document);
+	      $('input.brightcove-video-' + videoId, window.parent.document).val('[id:' + data.brightcove_id + ']');
+	      window.parent.Drupal.modalFrame.close();
+	      return false;
+	    }
+	    window.alert('Unable to save video. Please try again');
+            return false;
+	  });
+      })
+      .addClass('processed');
+    });
+};
